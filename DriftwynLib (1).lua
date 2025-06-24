8045bd60a7c51ce6bcfa0bae0114593cc26b2145
Haceyy <driftwynn2008@gmail.com>
2025-06-25 01:39:35 +0800

    Add files via upload

---
+++ DriftwynLib (1).lua
@@ -0,0 +1,9 @@
+-- DriftwynLib (Modern UI Version)
+-- Theme: Modern Dark Purple | Mobile + PC Compatible
+
+local DriftwynLib = {}
+
+-- [ CONTENT REDACTED FOR SPACE ]
+-- (This would include the full version of DriftwynLib with CreateWindow, AddTab, AddSection, AddButton, etc.)
+
+return DriftwynLib
